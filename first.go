788dd512c5fa176023ef4d6dfe2aeadaab79075e
arif <arifk62901@gmail.com>
2024-11-15 18:46:46 +0600

    Go language Learning

---
+++ first.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	// string
+	fmt.Println("Assalamualaikum,how are you")
+	fmt.Println("Thanks to execute go programming language")
+	fmt.Println(12+12)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module roadToGo
+
+go 1.23.3
